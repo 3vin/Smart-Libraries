64060687e10fa01d2eb5ee7c06e1edf9101a878a
Tevin Mhlanga <tevin.mhlanga@outlook.com>
2019-05-06 12:59:26 +0200

    Add Book app service

---
+++ aspnet-core/src/SmartLibraries.Application/Books/BookAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.AppFactory.Interfaces;
+using Abp.Domain.Repositories;
+using SmartLibraries.Books.Dto;
+
+namespace SmartLibraries.Books
+{
+    public class BookAppService : SmartLibrariesAsyncCrudAppServiceBase<Book, BookDto, long>
+    {
+        public BookAppService
+            (
+                IRepository<Book, long> repository,
+                ISyncHub syncHub
+            ) : base(repository, syncHub)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/SmartLibraries.Application/Books/Dto/BookDto.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace SmartLibraries.Books.Dto
+{
+    public class BookDto : EntityDto<long>
+    {
+        public string Author { get; set; }
+        public long Barcode { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long ISBN { get; set; }
+        public BookStatus Status { get; set; }
+    }
+}
