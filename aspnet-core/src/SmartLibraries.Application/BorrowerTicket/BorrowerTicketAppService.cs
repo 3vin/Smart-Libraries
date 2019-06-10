7bcc68a4ffdc5438b27b21a15638081ee89ec238
Tevin Mhlanga <tevin.mhlanga@outlook.com>
2019-06-10 09:35:38 +0200

    Add Borrower Ticket service and Dto

---
+++ aspnet-core/src/SmartLibraries.Application/BorrowerTicket/BorrowerTicketAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.AppFactory.Interfaces;
+using Abp.Domain.Repositories;
+using SmartLibraries.BookTicket.Dto;
+using SmartLibraries.BorrowerTickets;
+
+namespace SmartLibraries.BookTicket
+{
+    public class BorrowerTicketAppService : SmartLibrariesAsyncCrudAppServiceBase<BorrowerTicket, BorrowerTicketDto, long>
+    {
+        public BorrowerTicketAppService(
+            IRepository<BorrowerTicket, long> repository,
+            ISyncHub syncHub
+            ) : base(repository, syncHub)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/SmartLibraries.Application/BorrowerTicket/Dto/BorrowerTicketDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using SmartLibraries.Books.Dto;
+using SmartLibraries.BorrowerTickets;
+using System;
+using System.Collections.Generic;
+
+namespace SmartLibraries.BookTicket.Dto
+{
+    [AutoMap(typeof(BorrowerTicket))]
+    public class BorrowerTicketDto: EntityDto<long>
+    {
+        public DateTime BorrowDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        public long UserId { get; set; }
+        public List<BookDto> Books { get; set; }
+    }
+}
